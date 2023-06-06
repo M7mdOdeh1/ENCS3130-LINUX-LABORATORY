a113c0bd8b211831142e531d3988151614a3b952
M7md Odeh <1200089@student.birzeit.edu>
2023-06-06 10:57:04 +0300

    Update project.sh

--- Shell project/project.sh
+++ Shell project/project.sh
@@ -58,7 +58,7 @@ echo "Maximum received packets:$(echo "$rcv_packets" | tail -n 1)"
 sent_packets_info() {
 
 # it gathers the sent packets, removes the '/' sign, extracts the numerical values, and sorts them in ascending order
-rcv_packets=$( grep 'Networks:' "$1" | tr -s '/' ':' | cut -d: -f5 | sort -n)
+sent_packets=$( grep 'Networks:' "$1" | tr -s '/ ' ':' | cut -d: -f6 | sort -n)
 sum=0
 count=0
 
@@ -73,8 +73,21 @@ average=$(awk "BEGIN{printf \"%.2f\", $sum / $count}")
 echo "Average received packets: $average"
 echo "Minimum received packets:$(echo "$sent_packets" | head -n 1)"
 echo "Maximum received packets:$(echo "$sent_packets" | tail -n 1)"
-echo "$sent_packets"
+#echo "$sent_packets"
+}
 
+# function to extract all commands from the file
+extract_commands() {
+echo "extract_commands"
+commands=$(sed -n '/^[0-9]\{1,\}\>/,/^Process\>/p' "$1" | grep -v '^[0-9]\{1,\}\>' | grep -v '^Process\>' | grep -v '^top\>' | grep -v '^Tasks\>' | grep -v '^Cpu\>' | grep -v '^Mem\>' | grep -v '^Swap\>' | grep -v '^KiB\>' | grep -v '^PID\>' | grep -v '^USER\>' | grep -v '^PR\>' | grep -v '^NI\>' | grep -v '^VIRT\>' | grep -v '^RES\>' | grep -v '^SHR\>' | grep -v '^S\>' | grep -v '^%CPU\>' | grep -v '^%MEM\>' | grep -v '^TIME\>' | grep -v '^COMMAND\>')
+echo "$commands"
+}
+
+# extract commands with the maximum average CPU
+commands_with_max_cpu() {
+echo "commands_with_max_cpu"
+max_cpu_command=$(sed -n '/^[0-9]\{1,\}\>/,/^Process\>/p' top.txt)  
+echo "$max_cpu_command"
 }
 
 # start menu loop
@@ -109,9 +122,27 @@ case "$option"
 
   i) rcv_packets_info "$filename";;
   o) sent_packets_info "$filename";;
-  u);;
-  a);;
-  b);;
+  u) 
+    read -p "Enter an integer m: " m
+    
+    # Check if the input is an integer
+    while [ 1 ] 
+    do
+    if [[ $m =~ ^[0-9]+$ ]]; then
+        echo "The input is an integer."
+        extract_commands "$filename"
+        break
+    else
+        echo "The input is not an integer."
+    fi
+    read -p "Enter an integer m: " m
+    done
+
+    ;;
+  a) ;;
+    # check if m is an integer
+    
+  b) ;;
   e) exit 0;;
   *) echo "Please enter one of the choices only!!!";;
 esac
